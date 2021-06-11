24c84aa89e33b7c2b8a88c13ae73705a49dea32a
harshsri2208 <harshsri2208@gmail.com>
2021-06-11 20:05:04 +0530

    springboot project created

---
+++ backend/expense/src/main/java/com/harshsri2208/expense/ExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.harshsri2208.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseApplication.class, args);
+	}
+
+}
---
+++ backend/expense/src/test/java/com/harshsri2208/expense/ExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshsri2208.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
