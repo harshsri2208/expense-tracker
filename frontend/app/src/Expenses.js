94ad906cbca325c54c59a774507e1e099659ef29
harshsri2208 <harshsri2208@gmail.com>
2021-07-02 13:18:10 +0530

    added expense list and connections to rest api

--- frontend/app/src/Expenses.js
+++ frontend/app/src/Expenses.js
@@ -2,33 +2,129 @@ import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import "./App.css";
 import "react-datepicker/dist/react-datepicker.css";
-import { Container, Form, FormGroup, Button, Label, Input } from "reactstrap";
+import {
+  Table,
+  Container,
+  Form,
+  FormGroup,
+  Button,
+  Label,
+  Input,
+} from "reactstrap";
 import { Link } from "react-router-dom";
+import Moment from "react-moment";
 
 import AppNav from "./AppNav";
 
 class Expenses extends Component {
-  state = {
-    date: new Date(),
-    isLoading: true,
-    expenses: [],
-    Categories: [],
+  emptyItem = {
+    id: "103",
+    expensedate: new Date(),
+    description: "",
+    location: "",
+    category: [1, "Travel"],
   };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: new Date(),
+      isLoading: true,
+      Expenses: [],
+      Categories: [],
+      item: this.emptyItem,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind();
+    this.handleChange = this.handleChange.bind();
+    this.handleDateChange = this.handleDateChange.bind();
+  }
+
+  async handleSubmit(event) {
+    const item = this.state.item;
+
+    await fetch(`/api/expenses`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+
+    event.preventDefault();
+    this.props.history.push("/expenses");
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    let item = { ...this.state.item };
+    item[name] = value;
+    this.setState({ item });
+    console.log(item);
+  }
+
+  handleDateChange(date) {
+    let item = { ...this.state.item };
+    item.expensedate = date;
+    this.setState({ item });
+  }
+
+  async remove(id) {
+    await fetch(`/api/expenses/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      let updatedExpenses = [...this.state.Expenses].filter((i) => i.id !== id);
+      this.setState({ Expenses: updatedExpenses });
+    });
+  }
 
   async componentDidMount() {
     const response = await fetch("/api/categories");
     const body = await response.json();
     this.setState({ Categories: body, isLoading: false });
+
+    const responseExp = await fetch("/api/expenses");
+    const bodyExp = await responseExp.json();
+    this.setState({ Expenses: bodyExp, isLoading: false });
   }
 
   render() {
     const title = <h3>Add Expense</h3>;
-    const { Categories, isLoading } = this.state;
+    const { Categories } = this.state;
+    const { Expenses, isLoading } = this.state;
 
     if (isLoading) return <div>Loading......</div>;
 
     let optionList = Categories.map((category) => (
       <option id={category.id}>{category.name}</option>
+    ));
+
+    let rows = Expenses.map((expense) => (
+      <tr key={expense.id}>
+        <td>{expense.description}</td>
+        <td>{expense.location}</td>
+        <td>
+          <Moment date={expense.expensedate} format="DD/MM/YYYY" />
+        </td>
+        <td>{expense.category.name}</td>
+        <td>
+          <Button
+            size="sm"
+            color="danger"
+            onClick={() => this.remove(expense.id)}
+          >
+            Delete
+          </Button>
+        </td>
+      </tr>
     ));
 
     return (
@@ -62,7 +158,7 @@ class Expenses extends Component {
             <FormGroup>
               <Label for="city">Date</Label>
               <DatePicker
-                selected={this.state.date}
+                selected={this.state.item.expensedate}
                 onChange={this.handleDateChange}
               />
             </FormGroup>
@@ -83,6 +179,21 @@ class Expenses extends Component {
               </Button>
             </FormGroup>
           </Form>
+        </Container>{" "}
+        <Container>
+          <h3>Expense List</h3>
+          <Table className="mt-4">
+            <thead>
+              <tr>
+                <th width="30%">Description</th>
+                <th width="10%">Location</th>
+                <th> Date</th>
+                <th> Category</th>
+                <th width="10%">Action</th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </Table>
         </Container>
       </div>
     );
